ee2b5c08a7176cbf97e20e28dbb2babd0f12cf2d
Funso Joba <hrfunsojoba@gmail.com>
2022-04-11 08:38:09 +0100

    database migrations

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/product', function () {
+    return 'product';
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
